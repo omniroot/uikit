184bc8b538998d2a7aa89da7a6dfd343396e10e1
omniroot <omnirootofc@gmail.com>
2025-03-24 16:51:25 +0300

    Update Loader

--- Loader/Loader.tsx
+++ Loader/Loader.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import styles from "./Loader.module.css";
+import { AnimatePresence, motion } from "motion/react";
 
 interface ILoaderProps {
+	isShow?: boolean;
 	fullscreen?: boolean;
 	width?: number;
 	height?: number;
 }
-export const Loader: FC<ILoaderProps> = ({ fullscreen, width, height }) => {
+export const Loader: FC<ILoaderProps> = ({ isShow = true, fullscreen, width, height }) => {
 	if (fullscreen)
 		return (
 			<div className={styles.fullscreen_loader}>
@@ -14,5 +16,18 @@ export const Loader: FC<ILoaderProps> = ({ fullscreen, width, height }) => {
 			</div>
 		);
 
-	return <div className={styles.loader} style={{ width, height }} />;
+	return (
+		<AnimatePresence mode="wait">
+			{isShow && (
+				<motion.div
+					layout
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className={styles.loader}
+					style={{ width, height }}
+				/>
+			)}
+		</AnimatePresence>
+	);
 };
