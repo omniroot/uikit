efb158b9467e39553a0621e30338c273aa268ea8
omniroot <omnirootofc@gmail.com>
2025-04-07 15:23:39 +0300

    EXtract som etypes in UIProvider.types.ts

---
+++ UIProvider/UIProvider.types.ts
@@ -0,0 +1,15 @@
+export type IUIColor =
+	| "primary"
+	| "primary_container"
+	| "secondary"
+	| "secondary_container"
+	| "tertiary"
+	| "tertiary_container"
+	| "error"
+	| "error_container"
+	| "surface"
+	| "surface_container"
+	| "surface_container_high";
+
+export type IUISize = "large" | "medium" | "small";
+export type IUIFontVariant = "display" | "headline" | "title" | "body" | "label";
