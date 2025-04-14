d515017c3588972c141571928e589430260ff917
omniroot <omnirootofc@gmail.com>
2025-04-14 16:37:13 +0300

    Update PageTransition animation

--- PageTransition/PageTransition.tsx
+++ PageTransition/PageTransition.tsx
@@ -1,24 +1,43 @@
+import { useLocation } from "@tanstack/react-router";
 import { motion } from "motion/react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface IPageTransitionProps {
 	children: React.ReactNode;
 }
 export const PageTransition: FC<IPageTransitionProps> = ({ children }) => {
+	const [isDone, setIsDone] = useState(false);
+	const path = useLocation().pathname;
+
+	useEffect(() => {
+		setIsDone(false);
+	}, []);
+	console.log({ isDone });
+
+	useEffect(() => {
+		setIsDone(false);
+		setTimeout(() => {
+			setIsDone(true);
+		}, 200);
+	}, [path]);
+
 	const variants = {
-		initial: { opacity: 0.5 },
-		in: { opacity: 1 },
-		out: { opacity: 0 },
+		// initial: { opacity: 0.5, scale: 50 },
+		in: { scale: 1 },
+		out: { scale: 1.02 },
 	};
+
+	// if (isDone) return children;
 
 	return (
 		<motion.div
-			initial="initial"
-			animate="in"
+			// initial="initial"
+			animate={isDone ? "in" : "out"}
 			exit="out"
 			variants={variants}
-			transition={{ duration: 0.2 }}
+			transition={{ duration: 0.5, type: "spring" }}
 		>
+			{String(isDone)}
 			{children}
 		</motion.div>
 	);
